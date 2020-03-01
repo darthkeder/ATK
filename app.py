e229f61ef7bbf7721384fe16b8158a4a9613ac63
darthkeder <bimo.seto@gmail.com>
2020-03-01 14:55:47 +0700

    ganti app jadi application

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 from views import *
 
 if  __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    application.run(host='127.0.0.1', debug=True)
--- views.py
+++ views.py
@@ -1,10 +1,10 @@
-from app import app
+from app import application
 from flask import render_template, url_for, request, redirect, flash, session
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/penjualan')
+@application.route('/penjualan')
 def penjualan():
     return render_template('kasir_penjualan.html')
